26967cfd23b595b24e9cfa73f93c52c0527a758e
Daniel Sanchez <d.sanchez029@gmail.com>
2015-04-26 20:59:16 -0700

    Sunday Night

--- Final/physics.py
+++ Final/physics.py
@@ -7,13 +7,29 @@ class Physics:
         self.fieldDimenX = (self.ent.engine.gfxMgr.groundEnt.getBoundingBox().getSize().x) / 2
         self.fieldDimenZ = self.ent.engine.gfxMgr.groundEnt.getBoundingBox().getSize().z / 2
 
-        
+        self.entities = self.ent.engine.entityMgr.entities
+        self.var = 1
+
     def tick(self, dtime):
         #defined local var for fewer keystrokes
         # print "X: ", self.fieldDimenX, "Z: ", self.fieldDimenZ, "pos: ", self.ent.pos
 
         if (self.ent.mesh == "sphere.mesh"):
-            self.ent.pos.x += 5
+            for eid, entity in self.entities.iteritems():
+                if (entity != self):
+                    dist = self.distance(self.ent.pos.x, entity.pos.x, self.ent.pos.y, entity.pos.y)
+                #print "x2: ", x2, "z2: ", z2
+                #print dist
+                    if dist < 50:
+                        print "dist: ", dist
+                        #self.var *= -1
+            
+            self.ent.pos.x += (self.var * 10)
+            print "here"
+            if self.ent.pos.x > self.fieldDimenX:
+                self.ent.pos.x = 0
+
+
 
         if (self.ent.pos.x > self.fieldDimenX):
             self.ent.pos.x = self.ent.pos.x - 10
@@ -26,3 +42,8 @@ class Physics:
 
         if (self.ent.pos.z < -self.fieldDimenZ):
             self.ent.pos.z = self.ent.pos.z + 10
+
+    def distance(self, xi,xii,yi,yii):
+        sq1 = (xi-xii)*(xi-xii)
+        sq2 = (yi-yii)*(yi-yii)
+        return math.sqrt(sq1 + sq2)
